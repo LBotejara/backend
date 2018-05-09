f06ed84138601bf1dcf20ed80cbef6d1a71371ef
LBotejara <lsbdesarrollo@gmail.com>
2018-05-09 14:24:40 +0200

    Gráfico 2

--- routes/presupuesto.js
+++ routes/presupuesto.js
@@ -23,6 +23,24 @@ app.get('/', (req, res, next) => {
 
 });
 
+app.get('/cliente', (req, res, next) => {
+    
+        Presupuesto.aggregate([{$group:{_id:{cliente:"$cliente"},total:{$sum:"$total"}}}]).exec((err, datos)=>{
+            if(err){
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error acceso DB',
+                    errores: err
+                })
+            }
+            res.status(200).json({
+                ok: true,
+                datos: datos
+            })
+        });
+    
+    });
+
 app.get('/:id', function(req, res, next){
     
     Presupuesto.findById(req.params.id, (err, presupuesto)=>{
